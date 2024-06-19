c423eb938beb176e62df8907bc79fd814754af50
Davina Leong <davina.leong.sy@gmail.com>
2024-06-19 10:49:22 +0800

    Hide pot icon on large screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import LinkInterface from "./lib/link/LinkInterface"
 import Card from "./components/card/card"
 import potIcon from "./../assets/images/ib-favicon.svg"
+import "./page.scss"
 
 // TODO: Remove
 const link: LinkInterface = { label: "Chicken Wings", href: "#" }
@@ -10,13 +11,18 @@ const link: LinkInterface = { label: "Chicken Wings", href: "#" }
 export default function Home() {
   return (
     <main className="wrapper wrapper-centered q-container q-container-main bg-ibYellow-50 min-h-screen flow">
-      <h1 className="text-ibGreen-500 font-serif font-8xl font-black flex align-center gap-2">
-        <Link href="/" className="block">
+      <h1 className="text-ibGreen-500 font-serif text-4xl font-black flex align-center gap-2">
+        {`Welcome to Ivy's Bistro`}
+      </h1>
+
+      <p className="flex align-center gap-2">
+        <Link href="/" className="home-logo">
           <Image src={potIcon} alt="Pot icon" width="32" />
           <span className="sr-only">Home</span>
         </Link>
-        {`Welcome to Ivy's Bistro`}
-      </h1>
+        Tap on the cards to view the recipes.
+      </p>
+
       <div className="card-grid">
         <Card link={link} />
         <Card link={link} />
